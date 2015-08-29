99861b5df77efd009b808e7c401de03f1a46be36
vaxman <ulmann@vaxman.de>
2015-08-29 11:37:09 +0200

    Convincing git to take my changes

--- monitor/monitor.asm
+++ monitor/monitor.asm
@@ -1,11 +1,11 @@
-#define FPGA
-;;
+#undef FPGA
+;
 #include "sysdef.asm"
 #include "qmon.asm"
 #include "io_library.asm"
 #include "string_library.asm"
 #include "mem_library.asm"
 #include "debug_library.asm"
-;;
+;
 QMON$LAST_ADDR  HALT
 
