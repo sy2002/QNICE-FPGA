352144f04fc787a2ca1585db7714f63ee0349efb
vaxman <ulmann@vaxman.de>
2015-12-29 22:36:04 +0100

    VGA-test routine prints A-Z...

--- monitor/test_library.asm
+++ monitor/test_library.asm
@@ -22,9 +22,17 @@ _VGA$TEST_LOOP          MOVE    @R0, R8
                         RSUB    IO$PUTCHAR, 1
                         MOVE    @R1, R8
                         RSUB    IO$PUT_W_HEX, 1
+                        MOVE    ' ', R8
+                        RSUB    IO$PUTCHAR, 1
+                        MOVE    R2, R8
+                        RSUB    IO$PUT_W_HEX, 1
                         RSUB    IO$PUT_CRLF, 1
                         MOVE    0x0001, R8              ; Wait a moment
                         RSUB    MISC$WAIT, 1
                         MOVE    R2, R8
                         RSUB    VGA$PUTCHAR, 1          ; Print a single "A"-character
+                        ADD     0x0001, R2              ; Next character
+                        CMP     0x005B, R2              ; One after Z?
+                        RBRA    _VGA$TEST_LOOP, !Z      ; No, just continue
+                        MOVE    0x0041, R2
                         RBRA    _VGA$TEST_LOOP, 1
