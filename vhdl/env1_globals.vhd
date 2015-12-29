92ca44363c1e6596f35bc00877884ccc178fdac6
sy2002 <code@sy2002.de>
2015-12-29 22:17:20 +0100

    wip vga

--- vhdl/env1_globals.vhd
+++ vhdl/env1_globals.vhd
@@ -11,7 +11,7 @@ package env1_globals is
 
 -- file name and file size (in lines) of the file that is converted to the ROM located at 0x0000
 constant ROM_FILE             : string    := "../monitor/monitor.rom";
-constant ROM_SIZE             : integer   := 2418;
+constant ROM_SIZE             : integer   := 2407;
 
 -- size of lower register bank: should be 256
 -- set to 16 during development for faster synthesis, routing, etc.
