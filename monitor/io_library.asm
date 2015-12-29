b8cd455ddf7641d05baeabba0d4bec939972dfde
vaxman <ulmann@vaxman.de>
2015-12-29 22:43:23 +0100

    IO$PUTCHAR detects SW[1] to distinguish between UART/VGA output.

--- monitor/io_library.asm
+++ monitor/io_library.asm
@@ -208,17 +208,15 @@ IO$PUT_CRLF     INCRB                   ; Get a new register page
 ; TODO: This is highly unelegant and inefficient - it would be a better idea to
 ;       modify the address of the input routine to be used during a warm-start 
 ;       in order to avoid the many tests for the switch register.
+;
 IO$PUTCHAR          INCRB
-                    RSUB    UART$PUTCHAR, 1
-; Currently not active:
-;                    INCRB
-;                    MOVE    IO$SWITCH_REG, R0
-;                    MOVE    @R0, R1             ; Read the switch register
-;                    AND     0x0002, R1          ; Bit 1 set?
-;                    RBRA    _IO$PUTCHAR_UART, Z ; No, write to UART
-;                    RSUB    VGA$PUTCHAR, 1      ; Yes, write to VGA-controller
-;                    RBRA    _IO$PUTCHAR_END, 1  ; Finish
-;_IO$PUTCHAR_UART    RSUB    UART$PUTCHAR, 1
+                    MOVE    IO$SWITCH_REG, R0
+                    MOVE    @R0, R1             ; Read the switch register
+                    AND     0x0002, R1          ; Bit 1 set?
+                    RBRA    _IO$PUTCHAR_UART, Z ; No, write to UART
+                    RSUB    VGA$PUTCHAR, 1      ; Yes, write to VGA-controller
+                    RBRA    _IO$PUTCHAR_END, 1  ; Finish
+_IO$PUTCHAR_UART    RSUB    UART$PUTCHAR, 1
 _IO$PUTCHAR_END     DECRB
                     RET
 ;
