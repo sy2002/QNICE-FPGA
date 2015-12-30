98608b391e9714cbd7647aecb0fdf670540ae98a
vaxman <ulmann@vaxman.de>
2015-12-30 13:48:54 +0100

    Monitor works now with VGA and USB-keyboard.

--- monitor/vga_library.asm
+++ monitor/vga_library.asm
@@ -28,7 +28,7 @@ VGA$INIT                INCRB
                         MOVE    VGA$STATE, R0
                         MOVE    0x00E0, @R0             ; Enable everything
                         OR      VGA$COLOR_GREEN, @R0    ; Set font color to green
-;                        RSUB    VGA$CLS, 1              ; Clear the screen
+                        RSUB    VGA$CLS, 1              ; Clear the screen
                         XOR     R0, R0
                         MOVE    _VGA$X, R1
                         MOVE    R0, @R1                 ; Reset X coordinate
