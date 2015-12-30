35401a1457c0ca19cd89058f398ff4b4f51271d1
vaxman <ulmann@vaxman.de>
2015-12-30 11:10:24 +0100

    Removed output to serial line, removed wait in VGA-Test...

--- monitor/test_library.asm
+++ monitor/test_library.asm
@@ -16,20 +16,22 @@ VGA$TEST                RSUB    VGA$INIT, 1             ; Initialize VGA-interfa
                         MOVE    _VGA$X, R0              ; Setup pointers to the 
                         MOVE    _VGA$Y, R1              ; SW coordinates
                         MOVE    0x0041, R2              ; Print "A"s
-_VGA$TEST_LOOP          MOVE    @R0, R8
-                        RSUB    IO$PUT_W_HEX, 1         ; Print current coordinates
-                        MOVE    ' ', R8
-                        RSUB    IO$PUTCHAR, 1
-                        MOVE    @R1, R8
-                        RSUB    IO$PUT_W_HEX, 1
-                        MOVE    ' ', R8
-                        RSUB    IO$PUTCHAR, 1
-                        MOVE    R2, R8
-                        RSUB    IO$PUT_W_HEX, 1
-                        RSUB    IO$PUT_CRLF, 1
-                        MOVE    0x0001, R8              ; Wait a moment
-                        RSUB    MISC$WAIT, 1
-                        MOVE    R2, R8
+;_VGA$TEST_LOOP          MOVE    @R0, R8
+_VGA$TEST_LOOP          MOVE    R2, R8
+;                        MOVE    @R0, R8
+;                        RSUB    IO$PUT_W_HEX, 1         ; Print current coordinates
+;                        MOVE    ' ', R8
+;                        RSUB    IO$PUTCHAR, 1
+;                        MOVE    @R1, R8
+;                        RSUB    IO$PUT_W_HEX, 1
+;                        MOVE    ' ', R8
+;                        RSUB    IO$PUTCHAR, 1
+;                        MOVE    R2, R8
+;                        RSUB    IO$PUT_W_HEX, 1
+;                        RSUB    IO$PUT_CRLF, 1
+;                        MOVE    0x0001, R8              ; Wait a moment
+;                        RSUB    MISC$WAIT, 1
+;                        MOVE    R2, R8
                         RSUB    VGA$PUTCHAR, 1          ; Print a single "A"-character
                         ADD     0x0001, R2              ; Next character
                         CMP     0x005B, R2              ; One after Z?
