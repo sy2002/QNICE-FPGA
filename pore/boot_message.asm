bdc62294fb0add0d4f2ffba6931118b5a4bc96e4
sy2002 <code@sy2002.de>
2016-01-08 04:44:59 +0100

    new stable bitstream: 115.200 baud, 8-N-1, CTS and PORE

--- pore/boot_message.asm
+++ pore/boot_message.asm
@@ -2,4 +2,4 @@ PORE$NEWLINE  .ASCII_W "\n"
 
 ;PORE$RESETMSG .ASCII_W "QNICE-FPGA Version 1.2 by sy2002 in January 2016 (GIT COMMIT #"
 
-PORE$RESETMSG .ASCII_W "QNICE-FPGA [WIP] [only 16 reg-banks!] by sy2002 in January 2016 (GIT #77e8f6e)\n"
+PORE$RESETMSG .ASCII_W "QNICE-FPGA [WIP] [only 16 reg-banks!] by sy2002 in January 2016 (GIT #680813f)\n"
