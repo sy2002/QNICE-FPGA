fbe1f26593bd1d535c7e52cdd0885252e08de2f3
sy2002 <code@sy2002.de>
2016-05-05 11:44:25 +0200

    CPU/ALU change: introduced CMPU / CMPS

--- vhdl/alu.vhd
+++ vhdl/alu.vhd
@@ -1,7 +1,7 @@
 ----------------------------------------------------------------------------------
 -- QNICE CPU's ALU
 -- 
--- done in 2015 by sy2002
+-- done in summer 2015 and improved in May 2016 by sy2002
 ----------------------------------------------------------------------------------
 
 library IEEE;
@@ -56,7 +56,8 @@ end component;
 signal res : unsigned(16 downto 0);
 
 -- reuse adders, subtractors, ...
-signal r_sub : unsigned(16 downto 0);
+signal r_sub_u : unsigned(16 downto 0);
+signal r_sub_s : signed(15 downto 0);
 
 -- results from the shifter
 signal shifter_result : unsigned(15 downto 0);
@@ -78,7 +79,7 @@ begin
          x_out => shifter_x_out
       );
 
-   calculate : process (opcode, input1, input2, r_sub, c_in, shifter_result, shifter_c_out)
+   calculate : process (opcode, input1, input2, r_sub_u, c_in, shifter_result, shifter_c_out)
    begin
       case opcode is
          when opcMOVE =>
@@ -91,10 +92,10 @@ begin
             res <= ("0" & input2) + ("0" & input1) + ("0000000000000000" & c_in);
             
          when opcSUB =>
-            res <= r_sub;
+            res <= r_sub_u;
             
          when opcSUBC =>
-            res <= r_sub - ("0000000000000000" & c_in);
+            res <= r_sub_u - ("0000000000000000" & c_in);
 
          when opcSHL =>
             res <= shifter_c_out & shifter_result;
@@ -117,7 +118,10 @@ begin
          when opcXOR =>
             res <= ("0" & input1) xor ("0" & input2);
                      
-         when opcCMP =>
+         when opcCMPU =>
+            res <= "0" & input2;
+            
+         when opcCMPS =>
             res <= "0" & input2;
                      
          when others =>
@@ -125,14 +129,14 @@ begin
       end case;
    end process;
    
-   manage_flags : process (res, opcode, input1, input2, r_sub, shifter_x_out)
+   manage_flags : process (res, opcode, input1, input2, r_sub_u, r_sub_s, shifter_x_out)
    variable xres : unsigned(16 downto 0);
    begin
-      if opcode /= opcCMP then
-         xres := res;
-      else
-         xres := r_sub;
-      end if;
+      case opcode is
+         when opcCMPU => xres := r_sub_u;
+         when opcCMPS => xres := "0" & unsigned(r_sub_s);
+         when others =>  xres := res;
+      end case;
    
       -- the X register is context sensitive
       if opcode /= opcSHR then
@@ -159,7 +163,7 @@ begin
       -- V is true if adding/subtracting two negative numbers yields a positive
       -- number or if adding/subtracting two positive numbers yields a negative number
       if Opcode = opcADD or Opcode = opcADDC or Opcode = opcSUB or Opcode = opcSUBC or
-         Opcode = opcCMP or Opcode = opcAND or Opcode = opcOR or Opcode = opcXOR then
+         Opcode = opcCMPU or Opcode = opcCMPS or Opcode = opcAND or Opcode = opcOR or Opcode = opcXOR then
          if (input1(15) = '0' and input2(15) = '0' and xres(15) = '1') or
             (input1(15) = '1' and input2(15) = '1' and xres(15) = '0')
          then
@@ -174,7 +178,8 @@ begin
 
 
 
-r_sub <= ("0" & input2) - ("0" & input1);
+r_sub_u <= ("0" & input2) - ("0" & input1);
+r_sub_s <= signed(input2) - signed(input1);
 
 result <= res(15 downto 0);
 C <= res(16);
