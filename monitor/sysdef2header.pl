68485623715e7824effad68a05cd510ae7c994b5
vaxman <ulmann@vaxman.de>
2016-10-26 19:34:08 +0200

    Replace '$' by '_'.

--- monitor/sysdef2header.pl
+++ monitor/sysdef2header.pl
@@ -10,13 +10,14 @@ open my $output, '>', $hf or die "Could not open $hf: $!\n";
 while (my $line = <$input>)
 {
     next if $line =~ /^#define/;
-    $line =~ s/;/\/\//;
-    if ($line =~ /\.EQU/i)
+    $line =~ s/;/\/\//;     # Transform ';'-comments into '//'-comments
+    $line =~ s/\$/_/g;      # Get rid of dollar signs (I miss them! :-) )
+    if ($line =~ /\.EQU/i)  # This line contains an '.EQU', so we have to transform it
     {
         my ($label, $rest) = $line =~ /^(.*)\s+\.EQU\s+(.*)$/i;
         $line = "#define $label\t$rest\n";
     }
-    print $output $line;
+    print $output $line;    # Write line (possibly modified) to the output file
 }
 
 close $output;
