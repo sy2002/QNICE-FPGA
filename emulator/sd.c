a8ffad641786047855808ec59b6bed0e19570fbc
Bernd Ulmann <ulmann@vaxman.de>
2016-12-28 21:47:56 +0100

    Report SD-card type V2.

--- emulator/sd.c
+++ emulator/sd.c
@@ -119,7 +119,7 @@ unsigned int sd_read_register(unsigned int address)
       value = sd_error;
       break;
     case SD_CSR: /* More or less a dummy operation as there is no room for errors in the emulation. :-) */
-      value = 0;
+      value = 0x4000; /* Report card type SD V2. */
       break;
     default:
 #ifdef VERBOSE
