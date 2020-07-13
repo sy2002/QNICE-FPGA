87d091593bd717094f65bc6f77f9e52c94bb3672
sy2002 <code@sy2002.de>
2020-07-13 16:04:26 +0200

    MEGA65: Created specific PORE (message) and specific global constant file

--- pore/compile_pore.sh
+++ pore/compile_pore.sh
@@ -2,4 +2,11 @@
 cat boot_message.txt > boot_message.asm
 git rev-parse --short HEAD | tr -d '\n' >> boot_message.asm 
 echo ')\n"' >> boot_message.asm
+echo -n "Standard PORE: "
 ../assembler/asm pore.asm
+
+cat boot_message_mega65.txt > boot_message_mega65.asm
+git rev-parse --short HEAD | tr -d '\n' >> boot_message_mega65.asm 
+echo ')\n"' >> boot_message_mega65.asm
+echo -n "MEGA65 PORE:   "
+../assembler/asm pore_mega65.asm
