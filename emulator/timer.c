a04ce678f22e2d88c2966f56ceaf25c6d3152c75
Bernd Ulmann <ulmann@analogparadigm.com>
2020-08-02 18:44:10 +0200

    The timer threads did not feature thread-local interval variables.

--- emulator/timer.c
+++ emulator/timer.c
@@ -27,11 +27,13 @@
 #endif
 
 unsigned int timer_registers[NUMBER_OF_TIMERS * REG_PER_TIMER], // Global register variables
-             *interrupt_request,                                // This is mapped to the interrupt_request flag in qnice.c
-             *interrupt_address,                                // This is mapped to the interrupt_address in the emulator
-             *id;                                               // Timer id for each thread
+    *interrupt_request,                                         // This is mapped to the interrupt_request flag in qnice.c
+    *interrupt_address,                                         // This is mapped to the interrupt_address in the emulator
+    id[NUMBER_OF_TIMERS];                                       // Timer id for each thread
 
-pthread_t *thread_list;
+unsigned long long interval[NUMBER_OF_TIMERS];
+
+pthread_t thread_list[NUMBER_OF_TIMERS];
 
 void initializeTimerModule(unsigned int *request, unsigned int *address) {
     interrupt_request = request;
@@ -39,23 +41,10 @@ void initializeTimerModule(unsigned int *request, unsigned int *address) {
 
     for (unsigned int i = 0; i < NUMBER_OF_TIMERS * 3; timer_registers[i++] = 0);
 
-    if (!(thread_list = malloc(sizeof(pthread_t) * NUMBER_OF_TIMERS))) {
-        perror("timer: Out of memory creating the thread_list!");
-        exit(-1);
-    }
-
-    if (!(id = malloc(sizeof(unsigned int) * NUMBER_OF_TIMERS))) {
-        perror("timer: Out of memory creating the ids!");
-        exit(-1);
-    }
-
     for (unsigned int i = 0; i < NUMBER_OF_TIMERS; i++) {
         thread_list[i] = (pthread_t) 0;
         id[i] = i;
     }
-#ifdef DEBUG
-    printf("timer.c initialized.\n");
-#endif
 }
 
 unsigned int readTimerDeviceRegister(unsigned int address) {
@@ -63,28 +52,28 @@ unsigned int readTimerDeviceRegister(unsigned int address) {
 }
 
 void writeTimerDeviceRegister(unsigned int address, unsigned int value) {
+    unsigned long long duration;
+
     timer_registers[address] = value;
-#ifdef DEBUG
-    printf("timer: Written %04X to register %d\n", value, address);
-#endif
 
     for (unsigned int i = 0; i < NUMBER_OF_TIMERS; i++) {
         if (timer_registers[i * REG_PER_TIMER + REG_PRE] && 
             timer_registers[i * REG_PER_TIMER + REG_CNT] && 
             timer_registers[i * REG_PER_TIMER + REG_INT]) {
-            if (!thread_list[i]) {
+            if (thread_list[i]) // If the timer is being reconfigured, cancel and recreate it
+                pthread_cancel(thread_list[i]);
+
+            interval[i] = timer_registers[*id + REG_CNT] * timer_registers[*id + REG_PRE] * 10;
 #ifdef DEBUG
-                printf("timer: Timer %d was off, will now be activated.\n", i);
+            printf("timer: Timer %d was off, will now be activated for %llu.\n", i, duration);
 #endif
-                pthread_create(thread_list + i, NULL, (void *) timer, (void *) (id + i));
-            }
+            pthread_create(thread_list + i, NULL, (void *) timer, (void *) (id + i));
         } else {
             if (thread_list[i]) {
 #ifdef DEBUG
                 printf("timer: Timer %d was on, will now be deactivated.\n", i);
 #endif
                 pthread_cancel(thread_list[i]);
-                thread_list[i] = 0;
             }
         }
     }
@@ -92,7 +81,10 @@ void writeTimerDeviceRegister(unsigned int address, unsigned int value) {
 
 void timer(unsigned int *id) {  // This implements one of n timers.
     for (;;) {
-        usleep(timer_registers[*id + REG_CNT] * timer_registers[*id + REG_PRE] * 10);
+#ifdef DEBUG
+        printf("\tTimer %d instantiated! Interval = %llu\n", *id, interval[*id]);
+#endif
+        usleep(interval[*id]);
 #ifdef DEBUG
         printf("\tTimer %d triggered!\n", *id);
 #endif
