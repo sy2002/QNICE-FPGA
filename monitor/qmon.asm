b85719f1765574d779896083bbf04301e70ea9e7
Bernd Ulmann <ulmann@analogparadigm.com>
2020-08-12 23:52:33 +0200

    Changed version number to 1.6.

--- monitor/qmon.asm
+++ monitor/qmon.asm
@@ -596,7 +596,7 @@ QMON$LOAD_E1C   SUB     IO$HEX_NIBBLES, R10     ; get numeric representation of 
 ;* Strings
 ;***************************************************************************************
                 
-QMON$WELCOME    .ASCII_P    "\n\nSimple QNICE-monitor - Version 0.8 (Bernd Ulmann, sy2002, July 2020)\n"
+QMON$WELCOME    .ASCII_P    "\n\nSimple QNICE-monitor - Version 1.6 (Bernd Ulmann, sy2002, August 2020)\n"
 #ifdef RAM_MONITOR
                 .ASCII_P    "Running in RAM!\n"
 #endif
