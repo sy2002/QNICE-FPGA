c0dd0aa68e052b17928bf866a28a417a2c1820d0
sy2002 <code@sy2002.de>
2020-08-21 20:13:49 +0200

    MEGA65: New driver leads to basic 16bit HyperRAM test to pass

--- vhdl/hw/MEGA65/sim/sim_hram_mmio_mux.vhd
+++ vhdl/hw/MEGA65/sim/sim_hram_mmio_mux.vhd
@@ -64,10 +64,10 @@ begin
       end if;
    end process;
  
-   -- HyperRAM starts at FF60
+   -- HyperRAM starts at FFF0
    hram_control : process(addr, data_dir, data_valid)
    begin
-      if addr(15 downto 4) = x"FF6" then
+      if addr(15 downto 4) = x"FFF" then
          hram_en <= '1';
          hram_we <= data_dir and data_valid;
          hram_reg <= addr(3 downto 0);
