c7faceb0218bafc3a598a4c3553e99d142e8f131
sy2002 <code@sy2002.de>
2020-08-31 11:49:39 +0200

    VBCC: separated QNICE specific compiler backend from the default packaging: new folder c/qnice/compiler-backend and adjusted make files (issue #100)

--- monitor/compile_and_distribute.sh
+++ monitor/compile_and_distribute.sh
@@ -7,4 +7,4 @@ perl sysdef2header.pl sysdef.asm ../dist_kit/sysdef.h
 perl sysdef2header.pl -p QMON_EP_ monitor.def ../dist_kit/qmon-ep.h
 perl sysdef2header.pl -vasm sysdef.asm ../dist_kit/sysdef.vasm
 perl sysdef2header.pl -vasm monitor.def ../dist_kit/monitor.vdef
-cp ../dist_kit/sysdef.h ../c/vbcc/machines/qnice
+cp ../dist_kit/sysdef.h ../c/qnice/compiler-backend/
