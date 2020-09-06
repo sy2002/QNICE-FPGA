0bbf8b36354b2de747f48df1be0a0cb842534977
sy2002 <code@sy2002.de>
2020-09-06 04:08:12 +0200

    test_programs/fancy_bg.asm first version ready

--- test_programs/fancy_bg.asm
+++ test_programs/fancy_bg.asm
@@ -8,18 +8,55 @@
 #include "../dist_kit/sysdef.asm"
 #include "../dist_kit/monitor.def"
 
-                .ORG    0xE500                  ; start E500 to install
+                .ORG    0xE000                  ; start E000 to install
 
                 ; install the scanline ISR
                 MOVE    VGA$SCAN_INT, R0
-                MOVE    100, @R0++
-                MOVE    S_ISR, @R0                
+                MOVE    0, @R0++
+                MOVE    S_ISR, @R0   
+
+                ; install the timer ISR
+                MOVE    IO$TIMER_0_PRE, R0
+                MOVE    0x0064, @R0++           ; 1 ms base ...
+                MOVE    0x00C8, @R0++           ; 0xC8 = 200ms = 5 Hz
+                MOVE    T_ISR, @R0
 
                 MOVE    TITLE_STR, R8
                 SYSCALL(puts, 1)
                 SYSCALL(exit, 1)                ; back to monitor
 
-TITLE_STR       .ASCII_W "Fancy background installed. Call F000 to uninstall."
+                .ORG    0xE020                  ; call E020 to uninstall
+
+                MOVE    VGA$SCAN_ISR, R0        ; uninstall VGA scanline ISR
+                MOVE    0, @R0
+                MOVE    IO$TIMER_0_INT, R0      ; uninstall timer ISR
+                MOVE    0, @R0
+                MOVE    VGA$PALETTE_ADDR, R0
+                MOVE    VGA$PALETTE_DATA, R1
+                MOVE    0, @R0                  ; foregr. col. back to green
+                MOVE    0x03E0, @R1             
+                MOVE    16, @R0                 ; backgr. col. back to black
+                MOVE    0, @R1
+
+                SYSCALL(exit, 1)
+
+
+TITLE_STR       .ASCII_W "Fancy background installed. Call E020 to uninstall."
+
+; ----------------------------------------------------------------------------
+; Timer ISR
+; Set foreground color for VGA STDOUT to white 5 times per second to make
+; sure, that if any program changes it, it is changed back fast enough
+; ----------------------------------------------------------------------------
+
+T_ISR           INCRB
+
+                MOVE    VGA$PALETTE_ADDR, R0
+                MOVE    0, @R0++
+                MOVE    0xFFFF, @R0
+
+                DECRB
+                RTI
 
 ; ----------------------------------------------------------------------------
 ; Scanline ISR
@@ -28,50 +65,50 @@ TITLE_STR       .ASCII_W "Fancy background installed. Call F000 to uninstall."
 
 S_ISR_LINE      .DW 0x0000
 S_ISR_J         .DW 0x0000
+S_ISR_DELAY     .DW 0x0000
+S_ISR_MUL       .DW 0x0062
 
-S_ISR           INCRB
-
-                MOVE    VGA$PALETTE_ADDR, R0
-                MOVE    16, @R0++
-                MOVE    0x2222, @R0
-
-                DECRB
-                RTI
-
-
-S_ISR1           INCRB                           ; make sure, R8..R11 are not
+S_ISR           INCRB                           ; make sure, R8..R11 are not
                 MOVE    R8, R0                  ; changed in this ISR
                 MOVE    R9, R1
                 MOVE    R10, R2
-                MOVE    R11, R3
+                MOVE    R11, R3                
                 INCRB
 
-                MOVE    S_ISR_LINE, R0
-                MOVE    VGA$PALETTE_ADDR, R1
-                MOVE    VGA$PALETTE_DATA, R2
-                MOVE    S_ISR_J, R3
+                MOVE    S_ISR_LINE, R0          ; scanline where ISR happens 
+                MOVE    VGA$PALETTE_ADDR, R1    ; palette address register
+                MOVE    VGA$PALETTE_DATA, R2    ; palette data register
+                MOVE    S_ISR_J, R3             ; variable "j"
+                MOVE    S_ISR_DELAY, R4         ; loop var. for slowing down
 
-                ;MOVE    @R3, R8
-                ;ADD     @R0, R8
-                ;MOVE    62, R9
-                ;SYSCALL(mulu, 1)
-                MOVE    16, @R1                 ; background color
-                MOVE    0x2222, @R2
-                RBRA    gone, 1
+                MOVE    @R3, R8                 ; (scanline + j) * 62
+                ADD     @R0, R8
+                MOVE    S_ISR_MUL, R9
+                MOVE    @R9, R9
+                SYSCALL(mulu, 1)
 
-                ADD     1, @R3
-                
-                ; make sure the next ISR happens at the next line
-                ADD     1, @R0
+                MOVE    16, @R1                 ; pal. addr choose backg. col.
+                MOVE    R10, @R2                ; set palette to new color
+
+                ADD     1, @R4                  ; delay the "scrolling effect"
+                CMP     600, @R4
+                RBRA    S_NEXT, !Z
+                MOVE    0, @R4
+                ADD     1, @R3                  ; this var. does scrolling
+        
+                ; make sure the next ISR happens at the next scanline line
+S_NEXT          ADD     1, @R0
                 MOVE    @R0, R2
                 CMP     480, R2
                 RBRA    S_ISR_SETSL, !Z
                 XOR     R2, R2                  ; 480 reached? then back to 0
                 MOVE    R2, @R0
+                ;MOVE    S_ISR_MUL, R0          ; comment-in for psychodelics
+                ;ADD     1, @R0                 ; ditto
 S_ISR_SETSL     MOVE    VGA$SCAN_INT, R1
                 MOVE    R2, @R1                 ; next interrupt in next line
 
-gone            DECRB                           ; restore R8..R11
+                DECRB                           ; restore R8..R11
                 MOVE    R0, R8
                 MOVE    R1, R9
                 MOVE    R2, R10
