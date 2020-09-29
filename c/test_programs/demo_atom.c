23ec59997092b6547a0eeeb7ce69dc4f6ed21371
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2020-09-29 10:49:12 +0200

    Refactor demo_atom to use bitmap pointer update

--- c/test_programs/demo_atom.c
+++ c/test_programs/demo_atom.c
@@ -34,49 +34,58 @@ int main()
 
    MMIO(VGA_STATE) |= VGA_EN_SPRITE;      // Enable sprites
 
+   // Configure all bitmaps
+   for (int image=0; image<96; ++image)
+   {
+      sprite_set_palette(image, palette);
+      sprite_set_bitmap(image,  bitmaps[image]);
+   }
+
+   int image = 0;
    int offset_x = 100*256;
    int offset_y = 0;
 
    while (1)
    {
-      // Loop over all images
-      for (int image=0; image<24; ++image)
+      // Location of image
+      int pos_x = 320 + offset_x/256;
+      int pos_y = 240 + offset_y/256;
+
+      // Update image position
+      for (int sprite=0; sprite<4; ++sprite)
       {
-         int pos_x = 320 + offset_x/256;
-         int pos_y = 240 + offset_y/256;
-         for (int y=0; y<2; ++y)
+         int x=sprite%2;
+         int y=sprite/2;
+         sprite_set_position(sprite,   pos_x-32+x*32, pos_y-32+y*32);
+         sprite_set_bitmap_ptr(sprite, image*2+x+48*y);
+         sprite_set_config(sprite,     VGA_SPRITE_CSR_VISIBLE);
+
+      }
+
+      // Cycle through all bitmaps
+      image = (image+1) % 24;
+
+      // Wait 6 frames before updating image.
+      for (int f=0; f<6; ++f)
+      {
+         offset_y -= offset_x/256;
+         offset_x += offset_y/256;
+
+         while (MMIO(VGA_SCAN_LINE) > 480)
          {
-            for (int x=0; x<2; ++x)
+            if (MMIO(IO_UART_SRA) & 1)
             {
-               sprite_set_palette(2*y+x,  palette);
-               sprite_set_bitmap(2*y+x,   bitmaps[image*2+x+48*y]);
-               sprite_set_config(2*y+x,   VGA_SPRITE_CSR_VISIBLE);
-               sprite_set_position(+2*y+x, pos_x-32+x*32, pos_y-32+y*32);
+               unsigned int tmp = MMIO(IO_UART_RHRA);
+               goto end;
+            }
+            if (MMIO(IO_KBD_STATE) & KBD_NEW_ANY)
+            {
+               unsigned int tmp = MMIO(IO_KBD_DATA);
+               goto end;
             }
          }
-
-         // Wait 6 frames before updating image.
-         for (int f=0; f<6; ++f)
-         {
-            offset_y -= offset_x/256;
-            offset_x += offset_y/256;
-
-            while (MMIO(VGA_SCAN_LINE) > 480)
-            {
-               if (MMIO(IO_UART_SRA) & 1)
-               {
-                  unsigned int tmp = MMIO(IO_UART_RHRA);
-                  goto end;
-               }
-               if (MMIO(IO_KBD_STATE) & KBD_NEW_ANY)
-               {
-                  unsigned int tmp = MMIO(IO_KBD_DATA);
-                  goto end;
-               }
-            }
-            while (MMIO(VGA_SCAN_LINE) <= 480)
-            {}
-         }
+         while (MMIO(VGA_SCAN_LINE) <= 480)
+         {}
       }
    }
 
