0ca31563b9b18a2f97ee488481a172f972cd391b
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2020-10-12 08:44:58 +0200

    Add proper ending

--- c/test_programs/maze2d.c
+++ c/test_programs/maze2d.c
@@ -42,16 +42,43 @@ static void draw_menu()
       case 3 : color = 0x0300; break;  // COLOR_ORANGE
    }
    clrscr();
-   cputsxy(1, 10, "Welcome to this aMAZEing game!\0", color);
-   cputsxy(1, 12, "Press g to generate a new maze.\0", color);
-   cputsxy(1, 13, "Press 123 to change the level of the game.\0", color);
-   cputsxy(1, 14, "Move around with the keys WASD / HJKL / arrows.\0", color);
-   cputsxy(1, 15, "Press r to reset the current maze.\0", color);
-   cputsxy(1, 16, "Press x to get a hint.\0", color);
-   cputsxy(1, 17, "Press q to quit the game.\0", color);
-   cputsxy(1, 18, "Press m to return to this menu.\0", color);
+   int row = 5;
+   cputsxy(1, row,   "Welcome to this aMAZEing game!\0", color);
+   cputsxy(1, row+2, "Press g to generate a new maze.\0", color);
+   cputsxy(1, row+3, "Press 123 to change the level of the game.\0", color);
+   cputsxy(1, row+4, "Move around with the keys WASD / HJKL / arrows.\0", color);
+   cputsxy(1, row+5, "Press r to reset the current maze.\0", color);
+   cputsxy(1, row+6, "Press x to get a hint.\0", color);
+   cputsxy(1, row+7, "Press q to quit the game.\0", color);
+   cputsxy(1, row+8, "Press m to return to this menu.\0", color);
 } // end of draw_menu
 
+static void draw_ending()
+{
+   int color = 0;
+   switch (level)
+   {
+      case 1 : color = 0x0000; break;  // COLOR_LIGHT_GREEN
+      case 2 : color = 0x0400; break;  // COLOR_YELLOW
+      case 3 : color = 0x0300; break;  // COLOR_ORANGE
+   }
+
+   const char *end[] = {
+      " __     __                                              _   ",
+      " \\ \\   / /                                             | |  ",
+      "  \\ \\_/ /__  _   _    ___  ___  ___ __ _ _ __   ___  __| |  ",
+      "   \\   / _ \\| | | |  / _ \\/ __|/ __/ _` | '_ \\ / _ \\/ _` |  ",
+      "    | | (_) | |_| | |  __/\\__ \\ (_| (_| | |_) |  __/ (_| |  ",
+      "    |_|\\___/ \\__,_|  \\___||___/\\___\\__,_| .__/ \\___|\\__,_|  ",
+      "                                        | |                 ",
+      "                                        |_|                 ",
+      "                                                            "};
+   for (int i=0; i<9; ++i)
+   {
+      cputsxy(10, 15+i, end[i], color);
+   }
+   cputsxy(10, 30, "Press g to generate a new maze.\0", color);
+} // end of draw_ending
 
 static int game_update()
 {
@@ -65,7 +92,7 @@ static int game_update()
          break;
       case GAME_OVER:
          maze_draw(level, hint);
-         cputsxy(1, 38, "You escaped!\0", 0);
+         draw_ending();
          break;
    }
 
@@ -75,7 +102,7 @@ static int game_update()
       case '1' : level = 1; break;
       case '2' : level = 2; break;
       case '3' : level = 3; break;
-      case 'g' : if (gameState == MENU)
+      case 'g' : if (gameState != PLAYING)
                  {
                     clrscr();
                     maze_init();
