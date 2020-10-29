2e5172c3ca1a333bbe3d4b26740e17b5ef6c26d3
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2020-10-29 09:54:21 +0100

    Compiler interrupt bug

---
+++ c/test_programs/issue_177.c
@@ -0,0 +1,27 @@
+// This is a small test program to test Issue #177.
+// When compiled without optimization, it generates incorrect assembly.
+// When compiled with optimization, it generates correct assembly.
+
+#include <sysdef.h>
+
+long a = 0;
+long b = 0;
+
+// convenient mechanism to access QNICE's Memory Mapped IO registers
+#define MMIO( __x ) *((unsigned int volatile *) __x )
+
+static __interrupt void isr(void)
+{
+   if (!a)
+      return;
+   b += 1;
+}
+
+int main()
+{
+   // Enable timer interrupt @ 10000 Hz
+   MMIO(IO_TIMER_0_INT) = (unsigned int) isr;
+
+   return 0;
+}
+
