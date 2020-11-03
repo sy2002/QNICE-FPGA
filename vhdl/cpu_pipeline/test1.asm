fccb4d7ca98d08d5e2818298ae133a4af5e579b5
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2020-11-03 17:11:58 +0100

    Add test of all source addressing modes

--- vhdl/cpu_pipeline/test1.asm
+++ vhdl/cpu_pipeline/test1.asm
@@ -2,6 +2,8 @@
 ; It tests the following:
 ; MOVE R, R
 ; MOVE @R, R
+; MOVE @R++, R
+; MOVE @--R, R
 ; So far, no data dependencies are challenged. I.e. a register
 ; written to is not used until at least 4 instructions later (
 ; corresponding to the length of the pipeline).
@@ -37,9 +39,27 @@ MOVE  R1,  R5     ; Should put 0x0012 into R5
 MOVE  R2,  R6     ; Should put 0x0013 into R6
 MOVE  R3,  R7     ; Should put 0x0014 into R7
 
-; Noe test MOVE @R, R
+; Now test MOVE @R, R
 MOVE  @R0, R8     ; Should put 0x0F04 into R8  (hex for MOVE R15, R1)
 MOVE  @R1, R9     ; Should put 0x0F08 into R9  (hex for MOVE R15, R2)
 MOVE  @R2, R10    ; Should put 0x0F0C into R10 (hex for MOVE R15, R3)
 MOVE  @R3, R11    ; Should put 0x0010 into R11 (hex for MOVE R0,  R4)
+
+; Now test MOVE @R++, R
+MOVE  @R0++, R4   ; Should put 0x0F04 into R4 and 0x0012 into R0
+MOVE  @R1++, R5   ; Should put 0x0F08 into R5 and 0x0013 into R1
+MOVE  @R2++, R6   ; Should put 0x0F0C into R6 and 0x0014 into R2
+MOVE  @R3++, R7   ; Should put 0x0010 into R7 and 0x0015 into R3
+
+; This is just to reset R4-R7 before the next test
+MOVE  R0, R4      ; Should put 0x0012 into R4
+MOVE  R1, R5      ; Should put 0x0013 into R5
+MOVE  R2, R6      ; Should put 0x0014 into R6
+MOVE  R3, R7      ; Should put 0x0015 into R7
+
+; Now test MOVE @--R, R
+MOVE  @--R0, R4   ; Should put 0x0F04 into R4 and 0x0011 into R0
+MOVE  @--R1, R5   ; Should put 0x0F08 into R5 and 0x0012 into R1
+MOVE  @--R2, R6   ; Should put 0x0F0C into R6 and 0x0013 into R2
+MOVE  @--R3, R7   ; Should put 0x0010 into R7 and 0x0014 into R3
 
